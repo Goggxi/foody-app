4e1bca41d9b91b8f73d6c112172da6104a71531d
goggxi <goggxi@gmail.com>
2021-07-13 17:05:59 +0800

    add class constant and set function getRecipes on FoodRecipesApi interface

---
+++ app/src/main/java/com/goggxi/foody/Constant.kt
@@ -0,0 +1,11 @@
+package com.goggxi.foody
+
+
+class Constant {
+
+    companion object {
+        const val API_KEY = BuildConfig.API_KEY
+        const val BASE_URL = "https://api.spoonacular.com"
+    }
+
+}
---
+++ app/src/main/java/com/goggxi/foody/RemoteDataSource.kt
@@ -0,0 +1,4 @@
+package com.goggxi.foody
+
+class RemoteDataSource {
+}
