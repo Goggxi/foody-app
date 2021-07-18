771ee5e012943dba1a4f5d4e3b11020e5638c1d6
goggxi <goggxi@gmail.com>
2021-07-18 22:25:04 +0800

    add activity details and add on nav graph

---
+++ app/src/main/java/com/goggxi/foody/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.goggxi.foody.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.goggxi.foody.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
